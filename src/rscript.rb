15fa2ffd4db7588e251c795122459d37dc35cf2b
Zach Dennis <zach.dennis@gmail.com>
2013-08-23 21:30:23 -0400

    Adding a bin/interactive with an "r" and "c" command to interactively test the grammar to see it's syntax tree.

--- src/rscript.rb
+++ src/rscript.rb
@@ -3,6 +3,6 @@ end
 
 require 'term/ansicolor'
 
-require File.join(File.dirname(__FILE__), 'rscript/lexer')
-require File.join(File.dirname(__FILE__), 'rscript/parser_ext')
-require File.join(File.dirname(__FILE__), 'rscript/parser')
+load File.join(File.dirname(__FILE__), 'rscript/lexer.rb') unless RScript.const_defined? :Lexer
+load File.join(File.dirname(__FILE__), 'rscript/parser_ext.rb') unless RScript.const_defined? :ParserExt
+load File.join(File.dirname(__FILE__), 'rscript/parser.rb') unless RScript.const_defined? :Parser
